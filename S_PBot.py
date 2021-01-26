68c3304af618d788e0aaa9fb238c0544df157980
Rabah AOUN <Rabah.aoun@outlook.com>
2021-01-26 16:55:46 +0100

    ajout fichier python

---
+++ S_PBot.py
@@ -0,0 +1,80 @@
+import numpy as np #The Numpy numerical computing library
+import pandas as pd #The Pandas data science library
+import requests #The requests library for HTTP requests in Python
+import xlsxwriter #The XlsxWriter libarary for 
+import math #The Python math module
+from secrets import IEX_CLOUD_API_TOKEN
+
+stocks = pd.read_csv("sp_500_stocks.csv")
+symbol ="AAPL"
+api_url = f'https://sandbox.iexapis.com/stable/stock/{symbol}/quote?token={IEX_CLOUD_API_TOKEN}'
+data = requests.get(api_url).json()
+print (data['latestPrice'])
+print (data['marketCap'])
+
+my_columns = ['Ticker', 'Price','Market Capitalization', 'Number Of Shares to Buy']
+final_dataframe = pd.DataFrame(columns = my_columns)
+final_dataframe = final_dataframe.append(
+                                        pd.Series([symbol, 
+                                                   data['latestPrice'], 
+                                                   data['marketCap'], 
+                                                   'N/A'], 
+                                                  index = my_columns), 
+                                        ignore_index = True)
+"""
+for symbol in stocks['Ticker']:
+    api_url = f'https://sandbox.iexapis.com/stable/stock/{symbol}/quote?token={IEX_CLOUD_API_TOKEN}'
+    data = requests.get(api_url).json()
+    final_dataframe = final_dataframe.append(
+                                        pd.Series([symbol, 
+                                                   data['latestPrice'], 
+                                                   data['marketCap'], 
+                                                   'N/A'], 
+                                                  index = my_columns), 
+                                        ignore_index = True)
+"""
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+symbol_groups = list(chunks(stocks['Ticker'], 100))
+symbol_strings = []
+for i in range(0, len(symbol_groups)):
+    symbol_strings.append(','.join(symbol_groups[i]))
+    print(symbol_strings[i])
+
+
+for symbol_string in symbol_strings:
+#     print(symbol_strings)
+    batch_api_call_url = f'https://sandbox.iexapis.com/stable/stock/market/batch/?types=quote&symbols={symbol_string}&token={IEX_CLOUD_API_TOKEN}'
+    data = requests.get(batch_api_call_url).json()
+    
+    for symbol in symbol_string.split(','):
+        final_dataframe = final_dataframe.append(
+                                        pd.Series([symbol, 
+                                                   data[symbol]['quote']['latestPrice'], 
+                                                   data[symbol]['quote']['marketCap'], 
+                                                   'N/A'], 
+                                                  index = my_columns), 
+                                        ignore_index = True)
+
+print (final_dataframe)
+
+portfolio_size = input("Enter the value of your portfolio:")
+
+try:
+    val = float(portfolio_size)
+except ValueError:
+    print("That's not a number! \n Try again:")
+    portfolio_size = input("Enter the value of your portfolio:")
+
+position_size = float(portfolio_size) / len(final_dataframe.index)
+for i in range(0, len(final_dataframe['Ticker'])-1):
+    final_dataframe.loc[i, 'Number Of Shares to Buy'] = math.floor(position_size / final_dataframe['Price'][i])
+
+writer = pd.ExcelWriter('recommended_trades.xlsx', engine='xlsxwriter')
+final_dataframe.to_excel(writer, sheet_name='Recommended Trades', index = False)
+
+writer.save()
